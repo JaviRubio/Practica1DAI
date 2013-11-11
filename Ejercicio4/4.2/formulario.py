7884eb4b85c900f8918490d140ffc87244f667d4
Javier Rubio <jvrr20@gmail.com>
2013-11-11 02:09:41 +0100

    Update formulario.py

--- Ejercicio4/4.2/formulario.py
+++ Ejercicio4/4.2/formulario.py
@@ -1,6 +1,23 @@
 # encoding: utf-8
 # File: code.py
 
+# Practicas de Desarrollo de Aplicaciones para Internet (DAI)
+# Copyright (C) 2013 - Javier(jvr20@correo.ugr.es)
+#    
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#   
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#   
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import web
 import pymongo
 from web import form
